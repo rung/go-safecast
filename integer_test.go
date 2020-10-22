c42dacb1a4958b13ffaa49843bef91d9e08ab3ce
Hiroki Suezawa <suezawa@gmail.com>
2020-10-22 22:57:59 +0900

    add test for all funcs

--- integer_test.go
+++ integer_test.go
@@ -39,3 +39,79 @@ func TestInt32(t *testing.T) {
 		}
 	}
 }
+
+func TestInt16(t *testing.T) {
+	tests := []struct {
+		value int
+		wantInt16 int16
+		wantErr bool
+	}{
+		{
+			value: 32767,
+			wantInt16: 32767,
+			wantErr: false,
+		},
+		{
+			value: -32768,
+			wantInt16: -32768,
+			wantErr: false,
+		},
+		{
+			value: 32768,
+			wantErr: true,
+		},
+		{
+			value: -32769,
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		got, err := Int16(test.value)
+		if (test.wantErr && err == nil) || !test.wantErr && err != nil {
+			t.Errorf("err is wrong, value=%v, wantErr=%v, err=%v", test.value, test.wantErr, err)
+			continue
+		}
+		if got != test.wantInt16 {
+			t.Errorf("value is wrong, value=%v, wantInt16=%v, got=%v", test.value, test.wantInt16, got)
+		}
+	}
+}
+
+func TestInt8(t *testing.T) {
+	tests := []struct {
+		value int
+		wantInt8 int8
+		wantErr bool
+	}{
+		{
+			value: 127,
+			wantInt8: 127,
+			wantErr: false,
+		},
+		{
+			value: -128,
+			wantInt8: -128,
+			wantErr: false,
+		},
+		{
+			value: 128,
+			wantErr: true,
+		},
+		{
+			value: -129,
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		got, err := Int8(test.value)
+		if (test.wantErr && err == nil) || !test.wantErr && err != nil {
+			t.Errorf("err is wrong, value=%v, wantErr=%v, err=%v", test.value, test.wantErr, err)
+			continue
+		}
+		if got != test.wantInt8 {
+			t.Errorf("value is wrong, value=%v, wantInt8=%v, got=%v", test.value, test.wantInt8, got)
+		}
+	}
+}
