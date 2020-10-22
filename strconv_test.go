cb7263e178a79e6daf59df94ac76e0f7ccf83e2a
Hiroki Suezawa <suezawa@gmail.com>
2020-10-22 21:42:24 +0900

    add strconv_test.go

---
+++ strconv_test.go
@@ -0,0 +1,45 @@
+package safecast
+
+import "testing"
+
+func TestAtoi32(t *testing.T) {
+	tests := []struct {
+		value string
+		wantInt32 int32
+		wantErr bool
+	}{
+		{
+			value: "2147483647",
+			wantInt32: 2147483647,
+			wantErr: false,
+		},
+		{
+			value: "-2147483648",
+			wantInt32: -2147483648,
+			wantErr: false,
+		},
+		{
+			value: "2147483648",
+			wantErr: true,
+		},
+		{
+			value: "-2147483649",
+			wantErr: true,
+		},
+		{
+			value: "test",
+			wantErr: true,
+		},
+	}
+
+	for _, test := range tests {
+		got, err := Atoi32(test.value)
+		if (test.wantErr && err == nil) || !test.wantErr && err != nil {
+			t.Errorf("err is wrong, value=%v, wantErr=%v, err=%v", test.value, test.wantErr, err)
+			continue
+		}
+		if got != test.wantInt32 {
+			t.Errorf("value is wrong, value=%v, wantInt32=%v, got=%v", test.value, test.wantInt32, got)
+		}
+	}
+}
