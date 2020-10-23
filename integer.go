c537bb0e1f17f71deab00651572c3d03d69d0c8e
Hiroki Suezawa <suezawa@gmail.com>
2020-10-23 11:05:40 +0900

    Add comment

--- integer.go
+++ integer.go
@@ -3,6 +3,7 @@ package safecast
 import "errors"
 
 // Int32 converts int to int32 in a safe way.
+// You get error when the value is out of the 32-bit range.
 func Int32(i int) (int32, error) {
 	if i > 2147483647 || i < -2147483648 {
 		return 0, errors.New("int32 out of range")
@@ -11,6 +12,7 @@ func Int32(i int) (int32, error) {
 }
 
 // Int16 converts int to int16 in a safe way.
+// You get error when the value is out of the 16-bit range.
 func Int16(i int) (int16, error) {
 	if i > 32767 || i < -32768 {
 		return 0, errors.New("int16 out of range")
@@ -19,6 +21,7 @@ func Int16(i int) (int16, error) {
 }
 
 // Int8 converts int to int8 in a safe way.
+// You get error when the value is out of the 8-bit range.
 func Int8(i int) (int8, error) {
 	if i > 127 || i < -128 {
 		return 0, errors.New("int8 out of range")
